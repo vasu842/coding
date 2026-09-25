fe9e754e9d013603bad1684839214812fe32336c
k.vasudeva <vv5650965@gmail.com>
2026-09-25 08:13:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-// Mount the React Application to DOM
-const rootElement = document.getElementById('root');
-if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(<App />);
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
